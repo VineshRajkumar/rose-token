d4fd0849176671e1b96a7f6079201be054f36dd7
WhiteCat2023 <berndtdenniscanaya@gmail.com>
2025-07-10 12:00:38 +0800

    feat: removed Authprovider from web/src/main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,12 +4,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "@/router";
-import { AuthProvider } from "./protected-route/auth-context";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AuthProvider>
-			<RouterProvider router={router} />
-		</AuthProvider>
+		<RouterProvider router={router} />
 	</StrictMode>
 );
