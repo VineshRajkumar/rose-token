61bb8882245cd08bade3f0feb1fe9372df57db4a
WhiteCat2023 <berndtdenniscanaya@gmail.com>
2025-07-10 11:50:49 +0800

    feat: connected protected route to zustand

--- api/src/services/auth.services.ts
+++ api/src/services/auth.services.ts
@@ -44,7 +44,7 @@ return user;
 
 export const login = async ({email,password}:{email:string,password:string})=>{
 
-    const user= await prisma.user.findUnique({where:{email}});
+  const user= await prisma.user.findUnique({where:{email}});
 
   if (!user) {
     throw new Error("User not found");
--- web/src/protected-route/protected-route.tsx
+++ web/src/protected-route/protected-route.tsx
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from "react-router";
-import { useAuth } from "./auth-context";
+import { useAuthStore } from "@/stores/auth-store";
 
 export const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth();
+  const isAuthenticated  = useAuthStore((state) => state.isAuthenticated);
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
